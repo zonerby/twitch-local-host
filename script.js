7cbd28175693778ffe1c3e36f24bbe72aa5d5f48
zonerby <artyr4210@yandex.ru>
2019-02-19 05:19:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,96 @@
-function getReactInstance(element) {
-        for (const key in element) {
-            if (key.startsWith('__reactInternalInstance$')) {
-                return element[key];
-            }
-        }
+        function getReactInstance(element) {
+                for (const key in element) {
+                    if (key.startsWith('__reactInternalInstance$')) {
+                        return element[key];
+                    }
+                }
 
-    return null;
-}
-
-    function searchReactParents(node, predicate, maxDepth = 15, depth = 0) {
-        try {
-            if (predicate(node)) {
-                return node;
-            }
-        } catch (_) {}
-
-        if (!node || depth > maxDepth) {
             return null;
         }
 
-        const {'return': parent} = node;
-        if (parent) {
-            return searchReactParents(parent, predicate, maxDepth, depth + 1);
-        }
+            function searchReactParents(node, predicate, maxDepth = 15, depth = 0) {
+                try {
+                    if (predicate(node)) {
+                        return node;
+                    }
+                } catch (_) {}
 
-        return null;
-    }
-    function getCurrentPlayer() {
-        let player;
-        const PLAYER = '.player';
-        try {
-            const node = searchReactParents(
-                getReactInstance($(PLAYER)),
-                n => n.stateNode && n.stateNode.player
-            );
-            player = node.stateNode;
-        } catch (_) {}
+                if (!node || depth > maxDepth) {
+                    return null;
+                }
 
-        return player;
-    }
+                const {'return': parent} = node;
+                if (parent) {
+                    return searchReactParents(parent, predicate, maxDepth, depth + 1);
+                }
 
-function add_button(){
-        var cont = document.getElementsByClassName("tw-flex tw-flex-row")[1];
-        var host = document.createElement('button');
-        host.className = 'button button--icon-only float-left';
-        host.id = "host-button";
-        host.title = "Local host";
-        cont.appendChild(host);
-        host.onclick = add_input;
-    }
-
-    function close_inp(){
-        document.getElementById('host-input').remove();
-        add_button();
-    }
-
-    function add_input(){
-        var cont = document.getElementsByClassName("tw-flex tw-flex-row")[1];
-        var inp = document.createElement('input');
-        inp.className = 'tw-block tw-font-size-7 tw-input';
-        inp.autocapitalize = 'off';
-        inp.autocomplete = 'off';
-        inp.autocorrect = 'off';
-        inp.placeholder = 'Channel name';
-        inp.type = 'search';
-        inp.id = 'host-input';
-        inp.addEventListener("keyup", function(event) {
-            if (event.key === "Enter") {
-                 var pl = window.getCurrentPlayer();
-                 var channel = document.getElementById('host-input').value;
-                 pl.player.player.setChannel(channel);
+                return null;
             }
-        });
-        cont.appendChild(inp);
-        document.getElementById('host-button').remove();
-        setTimeout(close_inp, 30*1000);
-    }
-    function load(){
-        add_button();
-    }
+            function getCurrentPlayer() {
+                let player;
+                const PLAYER = '.player';
+                try {
+                    const node = searchReactParents(
+                        getReactInstance($(PLAYER)),
+                        n => n.stateNode && n.stateNode.player
+                    );
+                    player = node.stateNode;
+                } catch (_) {}
 
-    if(window.attachEvent) {
-    window.attachEvent('onload', load);
-} else {
-    if(window.onload) {
-        var curronload = window.onload;
-        var newonload = function(evt) {
-            curronload(evt);
-            load();
-        };
-        window.onload = newonload;
-    } else {
-        window.onload = load;
-    }
+                return player;
+            }
+
+        function add_button(){
+                var cont = document.getElementsByClassName("tw-flex tw-flex-row")[1];
+                var host = document.createElement('button');
+                host.className = 'button button--icon-only float-left';
+                host.id = "host-button";
+                host.title = "Local host";
+                cont.appendChild(host);
+                host.onclick = add_input;
+            }
+
+            function close_inp(){
+                document.getElementById('host-input').remove();
+                add_button();
+            }
+
+            function add_input(){
+                var cont = document.getElementsByClassName("tw-flex tw-flex-row")[1];
+                var inp = document.createElement('input');
+                inp.className = 'tw-block tw-font-size-7 tw-input';
+                inp.autocapitalize = 'off';
+                inp.autocomplete = 'off';
+                inp.autocorrect = 'off';
+                inp.placeholder = 'Channel name';
+                inp.type = 'search';
+                inp.id = 'host-input';
+                inp.addEventListener("keyup", function(event) {
+                    if (event.key === "Enter") {
+                         var pl = window.getCurrentPlayer();
+                         var channel = document.getElementById('host-input').value;
+                         pl.player.player.setChannel(channel);
+                    }
+                });
+                cont.appendChild(inp);
+                document.getElementById('host-button').remove();
+                setTimeout(close_inp, 30*1000);
+            }
+            function load(){
+                add_button();
+            }
+
+            if(window.attachEvent) {
+            window.attachEvent('onload', load);
+        } else {
+            if(window.onload) {
+                var curronload = window.onload;
+                var newonload = function(evt) {
+                    curronload(evt);
+                    load();
+                };
+                window.onload = newonload;
+            } else {
+                window.onload = load;
+            }
+        }
