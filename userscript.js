abfdbabd6777867d2304ca815991ea7ee5b8c603
zonerby <artyr4210@yandex.ru>
2019-02-19 13:59:10 +0300

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         twitch-local-host
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include         *://*.twitch.tv/*
+// @grant       none
+// @run-at      document-body
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement('script');
+    script.id = 'local-host';
+    script.type = 'text/javascript';
+    script.src = "https://cdn.jsdelivr.net/gh/zonerby/twitch-local-host@0.4/localhost.js";
+    document.head.appendChild(script);
+
+    var style = document.createElement('link');
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    style.href = "https://cdn.jsdelivr.net/gh/zonerby/twitch-local-host@0.4/style.css";
+    document.head.appendChild(style);
+})();
